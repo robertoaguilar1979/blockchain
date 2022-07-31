3de2d5e6304b9e97d99156b62788f46531c21c71
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-07-30 23:18:10 -0700

    running npx hardhat test on test dir

--- hardhat-tutorial/test/Token.js
+++ hardhat-tutorial/test/Token.js
@@ -4,6 +4,7 @@ const {
 
 describe("Token contract", function () {
     it("Deployment should assign the total supply of tokens to the owner", async function () {
+
         const [owner] = await ethers.getSigners();
 
         const Token = await ethers.getContractFactory("Token");
