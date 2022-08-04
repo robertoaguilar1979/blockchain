cba38b516ee64b0375b5a7111b17cd405485d4bb
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-08-04 14:38:25 -0700

    added new folder applications with firstApplication.sol file

---
+++ solidity/0.applications/firstApplication.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter {
+
+    uint public count;
+
+    function get() public view returns (uint){
+        return count;
+    }
+
+    function increment() public{
+        count += 1;
+
+    }
+    function decrement() public{
+        count -= 1;
+
+    }
+
+
+}
