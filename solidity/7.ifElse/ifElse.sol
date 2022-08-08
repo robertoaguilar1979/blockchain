b227bddada3d251e58ab53c6db9dce8d7b184964
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-08-08 14:47:22 -0700

    updated ifElse.sol file

--- solidity/7.ifElse/ifElse.sol
+++ solidity/7.ifElse/ifElse.sol
@@ -4,6 +4,19 @@ pragma solidity ^0.8.3;
 
 contract IfElse {
 
-   
+   function checkNumber(int _number) public pure returns(string memory) {
+    string memory str;
+
+    if(_number == 0) {
+        str = "number is zero";
+    }else if(_number > 0){
+        str = "number is greater than zero";
+    } else {
+        str = "number is a negative number";
+    }
+
+
+   return str;
+   }
 
 }
