9235a415e1bea62fb96be2640d375c9b6d96b7db
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-08-11 23:50:24 -0700

    added array in array.sol file

--- solidity/9.array/array.sol
+++ solidity/9.array/array.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.3;
 
 contract Array {
 
+    uint[3] public array = [0,1,2];
 
+    function insertArray (uint index, uint element) public {
+        array[index] = element;
+    }
     
    
 }
