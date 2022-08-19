6e80eb7f1043a38997df2033b34c08d4fd8a1070
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-08-18 22:49:53 -0700

    added new updated struct.sol in 10.struct folder in solidity directory

--- solidity/10.struct/struct.sol
+++ solidity/10.struct/struct.sol
@@ -2,7 +2,19 @@
 
 pragma solidity ^0.8.3;
 
-contract StructContract {
 
+struct student {
+    uint roll;
+    string name;
+    
+}
+contract StructContract {
+student public s1;
+
+constructor( uint _roll, string memory _name){
+    s1.roll = _roll;
+    s1.name= _name;
+    
+}
    
 }
