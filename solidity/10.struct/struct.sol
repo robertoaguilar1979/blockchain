9ca3323d7b7a7bcebb6f6e9f2064b6f7690eca2a
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-08-19 22:33:35 -0700

    finished struct.sol

--- solidity/10.struct/struct.sol
+++ solidity/10.struct/struct.sol
@@ -16,5 +16,11 @@ constructor( uint _roll, string memory _name){
     s1.name= _name;
     
 }
-   
+   function change(uint _roll, string memory _name)  public {
+    student memory new_student = student({
+        roll:_roll,
+        name:_name
+    });
+    s1=new_student;
+   }
 }
