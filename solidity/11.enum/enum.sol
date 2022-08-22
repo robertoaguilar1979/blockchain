2368003782d00e63e19d176daa1b9bb88b4c223f
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-08-21 22:38:20 -0700

    finished enum

--- solidity/11.enum/enum.sol
+++ solidity/11.enum/enum.sol
@@ -3,6 +3,21 @@
 pragma solidity ^0.8.3;
 
 
-contract enum {
+contract enumContract {
+    enum user { allowed, not_allowed, wait}
 
+    user public u1 = user.wait;
+    uint public lottery = 1000;
+     
+
+     function check() public {
+        if( u1==user.allowed){
+            lottery = 0;
+
+        }
+     }
+
+     function changeOwner() public{
+       u1 = user.allowed;
+     }
 }
