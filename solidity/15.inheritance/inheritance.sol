c9955ad2f5d6c4041be53171c981e796d1afb1bb
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-08-30 23:13:16 -0700

    working on inheritance.sol

--- solidity/15.inheritance/inheritance.sol
+++ solidity/15.inheritance/inheritance.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.3;
 
 
 contract inheritance {
+    string public stringOne;
+    address public manager;
 
+    constructor(){
+        stringOne="hello world";
+        manager= msg.sender;
+    }
     
 }
